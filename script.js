8851fcb6e1e6b937947e84c3edaf627116c788a0
filippo <filippo.brunnelleschi@gmail.com>
2025-04-12 17:07:32 +0200

    Finish grid + extra bugs

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
+//Variables
 const container = document.querySelector(".container");
 
+
+//Rows
 const rows = document.createElement("div");
+let rowLength = 10;
 rows.classList.add("rowin");
 container.appendChild(rows);
 
+
+//Size Input
+const sizeInput = document.querySelector("input");
+sizeInput.addEventListener("input", function(e) {
+
+    if (parseInt(e.target.value)<=20)
+    {
+        rowLength = parseInt(e.target.value);
+    }
+     //console.log(rowLength);
+     createDivs(rowLength);
+}
+
+)
+
+
+
 function createDivs(r) {
+
+   rows.innerHTML = '';
 
     const c= r;
 
@@ -15,6 +38,10 @@ function createDivs(r) {
         for (let i = 0; i < c; i++) {  
             const div = document.createElement("div");
             div.classList.add("cell");
+            div.style.width=`${600/r}px`;
+            div.style.height=`${600/r}px`;
+            div.style.margin=`${5/r}px`;
+
             row.appendChild(div);  
         }
         rows.appendChild(row);
@@ -22,4 +49,18 @@ function createDivs(r) {
     
 }
 
-createDivs(5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+createDivs(rowLength);
